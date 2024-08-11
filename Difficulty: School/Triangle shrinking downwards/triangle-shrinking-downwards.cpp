e02d7c090c627a03bef9a33f7950db2ba0ab52f8
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2024-08-11 10:56:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Triangle shrinking downwards/triangle-shrinking-downwards.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include<string>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string triDownwards(string S) {
+        // code here
+        string temp = S;
+        for(int i=0 ; i<temp.size()-1 ; i++){
+            temp[i] = '.' ;
+            S+=temp;
+        }
+        return S;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        
+        cin >> S;
+
+        Solution ob;
+        string ans=ob.triDownwards(S);
+        
+        for(int i=0 ; i<ans.length() ; i++)
+        {
+            cout<<ans[i];
+            if((i+1)%S.length()==0)
+                cout<<endl;
+        }
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
