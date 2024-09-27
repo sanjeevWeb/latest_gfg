c5b9ce5bad0692366aa87296bc99e273f71e749b
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2024-09-27 10:54:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/At least two greater elements/at-least-two-greater-elements.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> findElements(vector<int> arr) {
+        // Your code goes here
+        sort(arr.begin(),arr.end());
+        vector<int> output;
+        
+        for(int i=0;i<arr.size()-2;i++){
+            output.push_back(arr[i]);
+        }
+        
+        return output;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after the integer input
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        vector<int> res = ob.findElements(arr);
+
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+        // cout<<"~"<<endl;
+    }
+}
+// } Driver Code Ends
