d5a99f9c28b303d027f34ad04ef3ca96eb64a488
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2024-12-31 11:05:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
@@ -4,49 +4,44 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function template for C++
-class Solution{
-public:	
-		
-	string removeDups(string S) 
-	{
-	    // Your code goes here
-	     
-    unordered_map<char, int> exists;
-    string ans = "";
-    for (int i = 0; i < S.size(); i++) {
-        if (exists.find(S[i]) == exists.end()) {
-            ans.push_back(S[i]);
-            exists[S[i]]++;
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string &S) {
+        // Your code goes here
+        unordered_map<char, int> exists;
+        string ans = "";
+        for (int i = 0; i < S.size(); i++) {
+            if (exists.find(S[i]) == exists.end()) {
+                ans.push_back(S[i]);
+                exists[S[i]]++;
+            }
         }
+        return ans;
     }
-    return ans;
-	}
 };
 
 //{ Driver Code Starts.
 
+int main() {
 
-int main() 
-{
-   	
-
-   	ios_base::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(NULL);
     cout.tie(NULL);
-   
-   	int t;
-   	cin >> t;
-   	while(t--)
-   	{
-   		string s;
-   		cin >> s;
 
-   		
-   		Solution ob;
-   		cout << ob.removeDups(s) << "\n";
-   		
-   	}
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+
+        cout << "~"
+             << "\n";
+    }
 
     return 0;
 }
