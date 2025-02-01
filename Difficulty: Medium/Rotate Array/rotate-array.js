5596f079513e81a3d47e72fc364be8efe0e65d5b
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-02-01 10:42:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.js
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+        let arr = readLine().split(' ').map(x => parseInt(x));
+        let input_ar0 = readLine().split(' ').map(x => parseInt(x));
+        let d = input_ar0[0];
+        let obj = new Solution();
+        obj.rotateArr(arr, d);
+        let S = '';
+        for (let i = 0; i < arr.length; i++) {
+            S += arr[i];
+            S += ' ';
+        }
+        console.log(S);
+
+        console.log("~");
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {number[]} arr
+ * @param {number} d
+ */
+
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    rotateArr(arr, d) {
+        // code here
+        const duzina_niza = arr.length;
+        d = d % duzina_niza;
+        var arr1 = [];
+      
+        for(let i=0; i<arr.length; i++) {
+              if(i<d){
+                 arr1[i] = arr[i];
+              } else {
+                  arr[i - d] = arr[i];
+              } 
+             
+         }
+         for(let i=0; i<arr1.length; i++) {
+             arr[arr.length - arr1.length + i] = arr1[i];
+         }
+        
+    }
+}
