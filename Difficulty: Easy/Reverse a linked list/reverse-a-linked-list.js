ecd503b4ba9669e3b2a98d6304acb8d82c21540c
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-02-01 10:45:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.js
@@ -0,0 +1,92 @@
+//{ Driver Code Starts
+// Initial Template for javascript
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => { inputString += inputStdin; });
+
+process.stdin.on('end', _ => {
+    inputString =
+        inputString.trim().split('\n').map(string => { return string.trim(); });
+
+    main();
+});
+
+function readLine() { return inputString[currentLine++]; }
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+function printlist(head) {
+    let current = head;
+    let s = '';
+    while (current !== null) {
+        s += current.data + " ";
+        current = current.next;
+    }
+    console.log(s);
+}
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for (; i < t; i++) {
+
+        let input_ar1 = readLine().split(' ').map(x => parseInt(x));
+        let n = input_ar1.length;
+        let head = new Node(input_ar1[0]);
+        let tail = head;
+        for (let i = 1; i < n; i++) {
+            tail.next = new Node(input_ar1[i]);
+            tail = tail.next;
+        }
+        let obj = new Solution();
+        head = obj.reverseList(head);
+        printlist(head);
+        console.log("~");
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for javascript
+
+/**
+ * @param {Node} head
+ * @returns {Node}
+ */
+
+/*
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+    reverseList(head) {
+        // your code here
+        let current=head;
+        let pre=null;
+        let next;
+        while(current!==null)
+        {
+            next=current.next;
+            current.next=pre;
+            pre=current;
+            current=next;
+        }
+        return pre;
+    }
+}
