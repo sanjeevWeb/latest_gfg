c695162e3f84faf3a3bc705077a30a440806daed
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-03-04 19:29:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.py
@@ -0,0 +1,17 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+n = int(input())
+
+# Your code here
+for i in range(1,11):
+    print(i*n,end=" ")
+
+#{ 
+ # Driver Code Starts.
+
+# } Driver Code Ends
