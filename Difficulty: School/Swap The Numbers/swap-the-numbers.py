dee7959d06a126c75a33aaef7bd9c26158ccf017
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-03-06 10:07:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.py
@@ -0,0 +1,22 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+
+a = int(input())
+b = int(input())
+
+########### Write your code below ###############
+
+# Write Code to Swap
+temp = a
+a = b
+b = temp
+########### Write your code above ###############
+
+print(a, b)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
