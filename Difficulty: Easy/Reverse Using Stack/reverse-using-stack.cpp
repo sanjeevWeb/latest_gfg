916969ebeec687f4c2e3fd28ae38424918708aa4
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-04-07 11:17:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Using Stack/reverse-using-stack.cpp
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.cpp
@@ -1,46 +1,43 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-char* reverse(char *str, int len);
-int main()
-{
-    long long int t;
-    cin>>t;
-    while(t--)
-    {
-        char str[10000];
-        cin>>str;
-        long long int len=strlen(str);
-        char *ch=reverse(str,len);
-        for(int i=0;i<len;i++)
-        {
-            cout<<ch[i];
-        }
-        cout<<endl;
-    
-cout << "~" << "\n";
-}
-        return 0;
-}
 
 
 // } Driver Code Ends
 
+class Solution {
+  public:
+    string reverse(const string& S) {
+        // code here
+         stack < char> st ;
+        string ans = ""; 
+        for(int i=0 ; i<S.length() ;i++){
+            st.push(S[i]);;
+        }
+        for(int i=0 ; i<S.length() ;i++){
+            int element = st.top();
+            st.pop();
+            ans.push_back(element);
+        }
+        return ans ;
+    }
+};
 
-//return the address of the string
-char* reverse(char *S, int len)
-{
-    //code here
-    stack<char>st;
-    // char ans[len];
-    for(int i=0;i<len;i++){
-        st.push(S[i]);
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        Solution sol;
+        string input;
+        getline(cin, input);
+        string reversed = sol.reverse(input);
+        cout << reversed << endl;
+        cout << "~" << endl;
     }
-    // S.clear();
-    for(int i=0;i<len;i++){
-        S[i] = st.top();
-        st.pop();
-    }
-    
-    return S;
+    return 0;
 }
+// } Driver Code Ends
