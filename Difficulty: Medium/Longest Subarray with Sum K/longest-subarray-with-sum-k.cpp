4a069cd73c69f254cdaa9d6a5be17b917377e644
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-04-29 11:41:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -4,36 +4,35 @@ using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
     int longestSubarray(vector<int>& arr, int k) {
         // code here
-        unordered_map<int, int>
-            prefixSumIndices; // Maps cumulative sum to its earliest index
-        int currentSum = 0, longestLength = 0;
-
-        // Traverse through the array
-        for (int i = 0; i < arr.size(); i++) {
-            currentSum += arr[i]; // Accumulate the current sum
-
-            // Check if subarray starting from index 0 equals k
-            if (currentSum == k)
-                longestLength = i + 1;
-
-            // If (currentSum - k) exists in the map, update longestLength
-            if (prefixSumIndices.find(currentSum - k) != prefixSumIndices.end()) {
-                longestLength =
-                    max(longestLength, i - prefixSumIndices[currentSum - k]);
+        int ans  = 0;
+        int sum = 0;
+        unordered_map<int, int> mp;
+        
+        int i = 0;
+        for(int i =0; i<arr.size(); i++){
+            sum += arr[i];
+            if(sum == k){
+                ans = max(i+1, ans);
             }
-
-            // Store the current sum in the map if it hasn't been stored yet
-            if (prefixSumIndices.find(currentSum) == prefixSumIndices.end())
-                prefixSumIndices[currentSum] = i;
+            if(mp.find(sum - k) != mp.end()){
+                ans = max(ans, i- mp[sum-k]);
+            }
+            
+            if(mp.find(sum) == mp.end()){
+                mp[sum] = i;
+            }
+            
+            
         }
-
-        return longestLength; // Return the length of the longest subarray
+        return ans;
     }
 };
+
 
 //{ Driver Code Starts.
 
