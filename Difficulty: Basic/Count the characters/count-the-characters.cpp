568af141aad04a6f4adcda0c7261b8e3bcf747ef
Kumar Sanjeev Dutta <108945506+sanjeevWeb@users.noreply.github.com>
2025-05-03 11:17:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count the characters/count-the-characters.cpp
+++ Difficulty: Basic/Count the characters/count-the-characters.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 // } Driver Code Ends
 
-class Solution
-{
+
+class Solution {
   public:
-        int getCount (string S, int N)
-        {
-            //code here.
-            unordered_map<char,int>umap;
+    int getCount(string S, int N) {
+        // code here.
+        unordered_map<char,int>umap;
             
             for(int i=0;i<S.size();i++){
                if(S[i]!=S[i+1]){
@@ -26,10 +25,9 @@ class Solution
                 }
             }
             return total;
-            
-            
-        }
+    }
 };
+
 
 //{ Driver Code Starts.
 int main()
@@ -42,7 +40,9 @@ int main()
         
         Solution ob;
         cout <<ob.getCount (s, n) << endl;
-    }
+    
+cout << "~" << "\n";
+}
 }
 
 // Contributed By: Pranay Bansal
